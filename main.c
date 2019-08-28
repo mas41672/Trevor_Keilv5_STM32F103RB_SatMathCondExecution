54b626ff30762c27bdbdd09a6205d415510519c7
Dirk Possardt <dirk.possardt1@freenet.de>
2019-08-28 16:08:24 +0200

    chore: Trevor Keilv5 STM32F103RB Saturation and Conditional Execution
    This Files (Project) are tested in microVision V5.28.0.0 .
    The Pack with the examples can be downloaded from:
    https://booksite.elsevier.com/9780081006290/
    The example in the Pack is named:
    'Ex 3.1 Saturated maths and conditional execution'
    
    Files:
    
    Abstract.txt
    DebugConfig/Target_1_STM32F103RB.dbgconf
    EventRecorderStub.scvd
    RTE/Device/STM32F103RB/RTE_Device.h
    RTE/Device/STM32F103RB/RTE_Device.h.0000
    RTE/Device/STM32F103RB/RTE_Device.h.0001
    RTE/Device/STM32F103RB/RTE_Device.h.0002
    RTE/Device/STM32F103RB/startup_stm32f10x_md.s
    RTE/Device/STM32F103RB/system_stm32f10x.c
    RTE/_Target_1/RTE_Components.h
    Saturation.build_log.htm
    Saturation.htm
    Saturation.lnp
    Saturation.map
    Saturation.sct
    Saturation.uvguix.TMartin
    Saturation.uvoptx
    Saturation.uvprojx
    Saturation_Target 1.dep
    main.c
    main.crf
    main.d
    startup_stm32f10x_md.d
    startup_stm32f10x_md.o
    system_stm32f10x.crf
    system_stm32f10x.d

---
+++ main.c
@@ -0,0 +1,36 @@
+/*----------------------------------------------------------------------------
+	
+	Designers Guide to the Cortex-M Family
+	Saturation example
+
+*----------------------------------------------------------------------------*/
+
+#include "stm32f10x.h"
+#define Q_FLAG 0x08000000
+uint16_t a = 0;
+uint16_t range = 300;
+uint16_t locked = 0;
+uint16_t c = 0;
+uint32_t xPSR = 0;
+
+int main (void)
+{
+register unsigned int apsr __asm("apsr");
+	
+	while (1)
+	{
+		for(a=0;a<range;a++)					//a goes from 0 to 300
+		{
+			c = a;
+		//	c = __SSAT	(a, 9);				//c will saturate at 255
+		}
+/*	xPSR = 	__get_APSR ();
+		if(xPSR&Q_FLAG)	
+		{															//Using Compiler optimisation level 3, the if, then, else statement 
+			range--;										//will be constructed using conditional instructions
+		}	
+		apsr = apsr&~Q_FLAG;
+*/	
+	}
+}
+
